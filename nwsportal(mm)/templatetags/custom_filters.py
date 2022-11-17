f8148d4683a142b038c9327c8db66498511ed524
vladislav <109619125+conFusedSZNn@users.noreply.github.com>
2022-11-17 23:30:48 +0300

    Create custom_filters.py

---
+++ nwsportal(mm)/templatetags/custom_filters.py
@@ -0,0 +1,36 @@
+from django import template
+
+
+register = template.Library()
+
+
+@register.filter()
+def censor(message: str):
+   variants = ['mat', 'abc', 'def', 'no', 'filter']
+   ln = len(variants)
+   filtred_message = ''
+   string = ''
+   pattern = '*'
+   for i in message:
+       string += i
+       string2 = string.lower()
+
+       flag = 0
+       for j in variants:
+           if not string2 in j:
+               flag += 1
+           if string2 == j:
+               filtred_message += pattern * len(string)
+               flag -= 1
+               string = ''
+
+       if flag == ln:
+           filtred_message += string
+           string = ''
+
+   if string2 != '' and string2 not in variants:
+       filtred_message += string
+   elif string2 != '':
+       filtred_message += pattern * len(string)
+
+   return filtred_message
