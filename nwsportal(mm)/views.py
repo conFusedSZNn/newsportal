9dbb42d1df6dbc83b85a23b212ee8bf36c9842cb
vladislav <109619125+conFusedSZNn@users.noreply.github.com>
2022-11-17 23:28:00 +0300

    Update views.py

--- nwsportal(mm)/views.py
+++ nwsportal(mm)/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import AuthorUser, Category, Post, PostCategory, Comment
 
-# Create your views here.
+
+class PostList(ListView):
+    model = Post
+    ordering = 'one_to_many_post'
+    template_name = 'nwsportal.html'
+    context_object_name = 'news'
+
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'new.html'
+    context_object_name = 'new'
+    pk_url_kwarg = 'id'
