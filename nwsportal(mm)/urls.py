803a790702d5b3f3a30f376428b5374cbd39e53a
vladislav <109619125+conFusedSZNn@users.noreply.github.com>
2022-11-24 00:00:40 +0300

    Update urls.py

--- nwsportal(mm)/urls.py
+++ nwsportal(mm)/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import PostList, PostDetail, PostCreate,PostEdit,PostDelete, PostartCreate,PostartEdit,PostartDelete
+from .views import PostList, PostDetail, PostCreate,PostEdit,PostDelete, PostartCreate,PostartEdit,PostartDelete, SearchList
 
 
 urlpatterns = [
    path('', PostList.as_view(), name = 'post_list' ),
    path('<int:pk>', PostDetail.as_view(), name = 'post_details'),
-   path('search/', PostList.as_view(), name = 'post_search'),
+   path('search/', SearchList.as_view(), name = 'post_search'),
    path('create/', PostCreate.as_view(), name='post_create'),
    path('<int:pk>/edit/', PostEdit.as_view(), name='post_update'),
    path('<int:pk>/delete/', PostDelete.as_view(), name='post_delete'),
