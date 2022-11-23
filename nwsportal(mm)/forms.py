bdbba9d6e2f047db9ccd911d628a469ff65033b1
vladislav <109619125+conFusedSZNn@users.noreply.github.com>
2022-11-23 23:44:21 +0300

    Update forms.py

--- nwsportal(mm)/forms.py
+++ nwsportal(mm)/forms.py
@@ -1,9 +1,9 @@
 from django import forms
+
 from .models import Post
 
-
-
-class ClientDetailsForm(forms.ModelForm):
-    date_of_birth = DateField(input_formats=settings.DATE_INPUT_FORMATS)
+class PostForm(forms.ModelForm):
     class Meta:
-       model = Post
+        model = Post
+        fields = ['title', 'text']
+
