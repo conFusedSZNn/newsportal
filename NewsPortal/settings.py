aada1b9a522e85d7b13e688bfaea7c1835d53541
vladislav <109619125+conFusedSZNn@users.noreply.github.com>
2022-12-26 23:48:45 +0300

    Update settings.py

--- NewsPortal/settings.py
+++ NewsPortal/settings.py
@@ -160,8 +160,8 @@ STATICFILES_DIRS = [
 
 EMAIL_HOST = 'smtp.yandex.ru'  # адрес сервера Яндекс-почты для всех один и тот же
 EMAIL_PORT = 465  # порт smtp сервера тоже одинаковый
-EMAIL_HOST_USER = 'qqwqfnrr'  # ваше имя пользователя, например, если ваша почта user@yandex.ru, то сюда надо писать user, иными словами, это всё то что идёт до собаки
-EMAIL_HOST_PASSWORD = 'qwertyuiop21--'  # пароль от почты
+EMAIL_HOST_USER = '*****'  # ваше имя пользователя, например, если ваша почта user@yandex.ru, то сюда надо писать user, иными словами, это всё то что идёт до собаки
+EMAIL_HOST_PASSWORD = '*****'  # пароль от почты
 EMAIL_USE_SSL = True# Яндекс использует ssl, подробнее о том, что это, почитайте в дополнительных источниках, но включать его здесь обязательно
 
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER + '@yandex.ru'
