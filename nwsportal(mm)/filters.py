73ee6330e77020881d0b114481cae0d3cf806c92
vladislav <109619125+conFusedSZNn@users.noreply.github.com>
2022-12-28 17:38:02 +0300

    Update filters.py

--- nwsportal(mm)/filters.py
+++ nwsportal(mm)/filters.py
@@ -1,10 +1,20 @@
 from django_filters import FilterSet
-from .models import Post
+from .models import Post, Category
 import datetime
+
 
 class PostFilter(FilterSet):
    class Meta:
        model = Post
        fields = {
+
            'title': ['icontains'],
+       }
+
+class CategFilter(FilterSet):
+   class Meta:
+       model = Category
+       fields = {
+
+           'categ_name': ['icontains'],
        }
