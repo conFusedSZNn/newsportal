5ebc4b8f909e27c9baa016e9c15479216dd1a616
vladislav <109619125+conFusedSZNn@users.noreply.github.com>
2022-12-28 17:36:04 +0300

    Update urls.py

--- nwsportal(mm)/urls.py
+++ nwsportal(mm)/urls.py
@@ -1,15 +1,14 @@
 from django.urls import path
-from .views import PostList, PostDetail, PostCreate,PostEdit,PostDelete, PostartCreate,PostartEdit,PostartDelete, SearchList
+from nwsportal.views import PostList,PostDetail,PostCreate,PostEdit,PostDelete,SearchList, CategoryListView, subscribe
 
 
 urlpatterns = [
-   path('', PostList.as_view(), name = 'post_list' ),
-   path('<int:pk>', PostDetail.as_view(), name = 'post_details'),
-   path('search/', SearchList.as_view(), name = 'post_search'),
-   path('create/', PostCreate.as_view(), name='post_create'),
-   path('<int:pk>/edit/', PostEdit.as_view(), name='post_update'),
-   path('<int:pk>/delete/', PostDelete.as_view(), name='post_delete'),
-   path('create/', PostartCreate.as_view(), name='postart_create'),
-   path('<int:pk>/edit/', PostartEdit.as_view(), name='postart_update'),
-   path('<int:pk>/delete/', PostartDelete.as_view(), name='postart_delete'),
-]
+      path('', PostList.as_view(), name = 'post_list' ),
+      path('<int:pk>', PostDetail.as_view(), name = 'post_details'),
+      path('search/', SearchList.as_view(), name = 'post_search'),
+      path('create/', PostCreate.as_view(), name='post_create'),
+      path('<int:pk>/edit/', PostEdit.as_view(), name='post_update'),
+      path('<int:pk>/delete/', PostDelete.as_view(), name='post_delete'),
+      path('categories/<int:pk>', CategoryListView.as_view(), name='category_list'),
+      path('categories/<int:pk>/subscribe', subscribe, name='subscribe')
+         ]
