82a9bbad532a1fe7abfe36b262850b4eb738a06a
vladislav <109619125+conFusedSZNn@users.noreply.github.com>
2023-01-14 12:04:40 +0300

    Delete nwsportal(mm) directory

--- nwsportal(mm)/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import AuthorUser, Category, Post, PostCategory, Comment
-
-
-admin.site.register(AuthorUser)
-admin.site.register(Category)
-admin.site.register( Post)
-admin.site.register(PostCategory)
-admin.site.register(Comment)
--- nwsportal(mm)/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class NwsportalConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'nwsportal'
-
-    def ready(self):
-        import nwsportal.signals
