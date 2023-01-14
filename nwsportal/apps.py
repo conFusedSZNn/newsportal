72a3ec4f5da015687660a0f2efbdcbdf69ff10fb
vladislav <109619125+conFusedSZNn@users.noreply.github.com>
2023-01-14 12:09:19 +0300

    Create apps.py

---
+++ nwsportal/apps.py
@@ -0,0 +1,9 @@
+from django.apps import AppConfig
+
+
+class NwsportalConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'nwsportal'
+
+    def ready(self):
+        import nwsportal.signals
